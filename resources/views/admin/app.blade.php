4e0344f15af5f281b923ef33f6e24fd96aa4fd3f
peln <pern9378@stud.kea.dk>
2020-05-05 11:57:38 +0200

    scss mix setup + admin dashboard styles modified

--- resources/views/admin/app.blade.php
+++ resources/views/admin/app.blade.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>@yield('title') - {{ config('app.name') }}</title>
+    <title>@yield('title')</title>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="stylesheet" type="text/css" href="{{ asset('backend/css/main.css') }}" />
     <link rel="stylesheet" type="text/css" href="{{ asset('backend/css/font-awesome/4.7.0/css/font-awesome.min.css') }}"/>
+    <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
 </head>
 <body class="app sidebar-mini rtl">
     @include('admin.partials.header')
