ce89fca00d63935e083b25ed61daa624cb594738
peln <pern9378@stud.kea.dk>
2020-05-18 11:20:10 +0200

    login, register, profile, logout modified to project design

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -1,84 +1,109 @@
-
 <header class="section-header">
+	<nav class="navbar navbar-dark navbar-expand p-0 bg-primary">
+		<div class="container">
+			<ul class="navbar-nav d-none d-md-flex mr-auto">
+				<li class="nav-item"><a class="nav-link" href="#">Home</a></li>
+				<li class="nav-item"><a class="nav-link" href="#">Delivery</a></li>
+				<li class="nav-item"><a class="nav-link" href="#">Payment</a></li>
+			</ul>
+			<ul class="navbar-nav">
+				<li  class="nav-item"><a href="#" class="nav-link"> Call: +99812345678 </a></li>
+				<li class="nav-item dropdown">
+					<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown"> English </a>
+					<ul class="dropdown-menu dropdown-menu-right" style="max-width: 100px;">
+						<li><a class="dropdown-item" href="#">Arabic</a></li>
+						<li><a class="dropdown-item" href="#">Russian </a></li>
+						<li><a class="dropdown-item" href="#">Danish </a></li>
+						<li><a class="dropdown-item" href="#">Spanish </a></li>
+						<li><a class="dropdown-item" href="#">German </a></li>
+						<li><a class="dropdown-item" href="#">French </a></li>
+					</ul>
+				</li>
 
-<nav class="navbar navbar-dark navbar-expand p-0 bg-primary">
-<div class="container">
-    <ul class="navbar-nav d-none d-md-flex mr-auto">
-		<li class="nav-item"><a class="nav-link" href="#">Home</a></li>
-		<li class="nav-item"><a class="nav-link" href="#">Delivery</a></li>
-		<li class="nav-item"><a class="nav-link" href="#">Payment</a></li>
-    </ul>
-    <ul class="navbar-nav">
-		<li  class="nav-item"><a href="#" class="nav-link"> Call: +99812345678 </a></li>
-		<li class="nav-item dropdown">
-		 	<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown"> English </a>
-		    <ul class="dropdown-menu dropdown-menu-right" style="max-width: 100px;">
-				<li><a class="dropdown-item" href="#">Arabic</a></li>
-				<li><a class="dropdown-item" href="#">Russian </a></li>
-				<li><a class="dropdown-item" href="#">Danish </a></li>
-				<li><a class="dropdown-item" href="#">Spanish </a></li>
-				<li><a class="dropdown-item" href="#">German </a></li>
-				<li><a class="dropdown-item" href="#">French </a></li>
-		    </ul>
-		</li>
+				{{-- <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+					Logout
+				</a>
+				<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+					{{ csrf_field() }}
+				</form> --}}
 
-		{{-- <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-			Logout
-		</a>
-		<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-			{{ csrf_field() }}
-		</form> --}}
-
-	</ul> <!-- list-inline //  -->
-  </div> <!-- navbar-collapse .// -->
-</div> <!-- container //  -->
+			</ul> <!-- list-inline //  -->
+		</div> <!-- navbar-collapse .// -->
+	</div> <!-- container //  -->
 </nav> <!-- header-top-light.// -->
 
 <section class="header-main border-bottom">
 	<div class="container">
-<div class="row align-items-center">
-	<div class="col-lg-2 col-6">
-		<a href="/" class="brand-wrap">
-			<img class="logo" src="{{ asset('storage/soundstationlogo.jpg') }}">
-		</a> <!-- brand-wrap.// -->
-	</div>
-
-	<div class="col-lg-6 col-12 col-sm-12">
-		{{-- <form> --}}
-			<div class="input-group w-100">
-				<input type="text" id="txtSearch" class="form-control" placeholder="Search" minlength="2" maxlength="50">
-				<div class="input-group-append">
-					<button class="btn btn-primary" id="searchBtn" type="submit">
-					<i class="fa fa-search"></i>
-					</button>
-				</div>
+		<div class="row align-items-center">
+			<div class="col-lg-2 col-6">
+				<a href="/" class="brand-wrap">
+					<img class="logo" src="{{ asset('storage/soundstationlogo.jpg') }}">
+				</a> <!-- brand-wrap.// -->
 			</div>
-		{{-- </form> --}}
-		<div id="searchResults"></div>
-	</div> <!-- col.// -->
 
-	<div class="col-lg-4 col-sm-6 col-12">
-		<div class="widgets-wrap float-md-right">
-        <div class="widget-header icontext">
-				<a href="#" class="icon icon-sm rounded-circle border"><i class="fa fa-user"></i></a>
-				<div class="text">
-					<!-- <span class="text-muted">Welcome!</span> -->
-					<div> 
-						<a href="/login">Sign in</a>
-						{{-- |  <a href="/register"> Register</a> --}}
+			<div class="col-lg-6 col-12 col-sm-12">
+				{{-- <form> --}}
+					<div class="input-group w-100">
+						<input type="text" id="txtSearch" class="form-control" placeholder="Search" minlength="2" maxlength="50">
+						<div class="input-group-append">
+							<button class="btn btn-primary" id="searchBtn" type="submit">
+							<i class="fa fa-search"></i>
+							</button>
+						</div>
 					</div>
-				</div>
-            </div>
-            <div class="widget-header">
-				<a href="#" class="icon icon-sm rounded-circle border"><i class="fa fa-heart"></i></a>
-			</div>
-			<div class="widget-header  mr-3">
-				<a href="#" class="icon icon-sm rounded-circle border"><i class="fa fa-shopping-cart"></i></a>
-				<span class="badge badge-pill badge-danger notify">0</span>
-            </div>
-		</div> <!-- widgets-wrap.// -->
-	</div> <!-- col.// -->
-</div> <!-- row.// -->
+				{{-- </form> --}}
+				<div id="searchResults"></div>
+			</div> <!-- col.// -->
+
+			<div class="col-lg-4 col-sm-6 col-12">
+				<div class="widgets-wrap float-md-right">
+					<div class="widget-header icontext">
+						<ul class="navbar-nav ml-auto">
+							<!-- Authentication Links -->
+							@guest
+							<li class="nav-item dropdown">
+								<a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+									<span href="#" class="icon icon-sm rounded-circle border"><i class="fa fa-user"></i></span>
+								</a>
+								<div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+									<a class="dropdown-item" href="{{ route('login') }}">{{ __('Login') }}</a>
+									@if (Route::has('register'))
+										<a class="dropdown-item" href="{{ route('register') }}">{{ __('Register') }}</a>
+									@endif
+								</div>
+							</li>
+							@else
+								<li class="nav-item dropdown">
+									<a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+										<span href="#" class="icon icon-sm rounded-circle border"><i class="fa fa-user"></i></span>
+										{{ Auth::user()->name }} <span class="caret"></span>
+
+									</a>
+									<div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+										<a class="dropdown-item" href="{{ route('logout') }}"
+										onclick="event.preventDefault();
+											document.getElementById('logout-form').submit();">
+											{{ __('Logout') }}
+										</a>
+
+										<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+											@csrf
+										</form>
+									</div>
+								</li>
+							@endguest
+						</ul>
+					</div>
+					<div class="widget-header">
+						<a href="#" class="icon icon-sm rounded-circle border"><i class="fa fa-heart"></i></a>
+					</div>
+					<div class="widget-header  mr-3">
+						<a href="/cart" class="icon icon-sm rounded-circle border"><i class="fa fa-shopping-cart"></i></a>
+						<span class="badge badge-pill badge-danger notify">0</span>
+					</div>
+				</div> <!-- widgets-wrap.// -->
+			</div> <!-- col.// -->
+		</div> <!-- row.// -->
 	</div> <!-- container.// -->
 </section> <!-- header-main .// -->
 </header> <!-- section-header.// -->
