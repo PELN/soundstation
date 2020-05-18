d2b53023545bb8225143fd7010e73459d8fc4bb6
peln <pern9378@stud.kea.dk>
2020-05-18 10:41:49 +0200

    moved app.blade

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -22,12 +22,12 @@
 		    </ul>
 		</li>
 
-		<a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+		{{-- <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
 			Logout
 		</a>
 		<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
 			{{ csrf_field() }}
-		</form>
+		</form> --}}
 
 	</ul> <!-- list-inline //  -->
   </div> <!-- navbar-collapse .// -->
