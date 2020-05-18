85dbe233afd798b8f960e4d1cba6d4a46ca4345b
peln <pern9378@stud.kea.dk>
2020-05-18 15:35:33 +0200

    add to cart functionality works

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Validator;
 
 class CartController extends Controller
 {
@@ -11,4 +12,21 @@ class CartController extends Controller
         // dd($cartCollection);
         return view('pages.cart')->withTitle('E-COMMERCE STORE | CART')->with(['cartCollection' => $cartCollection]);;
     }
+
+    public function add(Request $request){
+
+        \Cart::add(array(
+            'id' => $request->id,
+            'name' => $request->name,
+            'price' => $request->price,
+            'quantity' => $request->quantity,
+            'attributes' => array(
+                'image' => $request->img,
+                'slug' => $request->slug
+            )
+        ));
+        return redirect()->route('cart.index')->with('success_msg', 'Item is Added to Cart!');
+    }
+
+    
 }
--- resources/views/pages/product_listing.blade.php
+++ resources/views/pages/product_listing.blade.php
@@ -138,39 +138,57 @@
 					<div class="row">
 						@foreach ($products as $product)
 						<div class="col-md-4">
-						<a href="{{ URL::to("{$category->category_slug}/{$product->slug}") }}">
-							<figure class="card card-product-grid">
-								<div class="img-wrap">
-									<!-- <span class="badge badge-danger"> NEW </span> -->
-									@if($product->path)
-										<img src="{{ asset('storage/product-images/'.$product->path) }}">
-									@else
-										<img src="{{ asset('storage/product-images/image-coming-soon.jpg') }}">
-									@endforelse
-								</div> <!-- img-wrap.// -->
-								<figcaption class="info-wrap">
-									<div class="fix-height">
-										<a href="#" class="title">{{$product->name}}</a>
-										<p class="artist">{{$product->artist}}</p>
-										<div class="price-wrap mt-2">
-											<span class="price">DKK {{$product->price}}</span>
-											{{-- <del class="price-old">$1980</del> --}}
-										</div> <!-- price-wrap.// -->
-									</div>
-									{{-- <a href="#" class="btn btn-block btn-primary">Add to cart </a> --}}
-									<div class="form-row">
-										<div class="col">
-											<a href="#" class="btn  btn-primary w-100"> <span class="text">Add to cart</span> <i class="fas fa-shopping-cart"></i>  </a>
-										</div> <!-- col.// -->
-										<div class="col">
-											<a href="#" class="btn  btn-light"> <i class="fas fa-heart"></i></a>
-										</div> <!-- col.// -->
-									</div> <!-- form row.// -->
-								</figcaption>
-							</figure>
+							<a href="{{ URL::to("{$category->category_slug}/{$product->slug}") }}">
+								<figure class="card card-product-grid">
+									<div class="img-wrap">
+										<!-- <span class="badge badge-danger"> NEW </span> -->
+										@if($product->path)
+											<img src="{{ asset('storage/product-images/'.$product->path) }}">
+										@else
+											<img src="{{ asset('storage/product-images/image-coming-soon.jpg') }}">
+										@endforelse
+									</div> <!-- img-wrap.// -->
+									<figcaption class="info-wrap">
+										<div class="fix-height">
+											<a href="#" class="title">{{$product->name}}</a>
+											<p class="artist">{{$product->artist}}</p>
+											<div class="price-wrap mt-2">
+												<span class="price">DKK {{$product->price}}</span>
+												{{-- <del class="price-old">$1980</del> --}}
+											</div> <!-- price-wrap.// -->
+										</div>
+										{{-- <a href="#" class="btn btn-block btn-primary">Add to cart </a> --}}
+										<div class="form-row">
+											<div class="col">
+												<button class="btn btn-primary w-100"> <span class="text">Add to cart</span> <i class="fas fa-shopping-cart"></i></button>
+											</div> <!-- col.// -->
+											<div class="col">
+												<a href="#" class="btn btn-light"> <i class="fas fa-heart"></i></a>
+											</div> <!-- col.// -->
+										</div> <!-- form row.// -->
+									</figcaption>
+								</figure>
 							</a>
-						</div> <!-- col.// -->
-						@endforeach
+
+							<form action="{{ route('cart.store') }}" method="POST">
+								{{ csrf_field() }}
+								<input type="hidden" value="{{ $product->id }}" id="id" name="id">
+								<input type="hidden" value="{{ $product->name }}" id="name" name="name">
+								<input type="hidden" value="{{ $product->price }}" id="price" name="price">
+								<input type="hidden" value="{{ $product->path }}" id="img" name="img">
+								<input type="hidden" value="{{ $product->slug }}" id="slug" name="slug">
+								<input type="hidden" value="1" id="quantity" name="quantity">
+								<div class="card-footer" style="background-color: white;">
+									<div class="row">
+										<button class="btn btn-secondary btn-sm" class="tooltip-test" title="add to cart">
+											<i class="fa fa-shopping-cart"></i> add to cart
+										</button>
+									</div>
+								</div>
+							</form>
+
+							</div> <!-- col.// -->
+							@endforeach
 					</div> <!-- row end.// -->
 				</div>
 
